459cff369ada4c2139715a363e8b68bc2e7eaa17
linjohnss <a0920594352@gmail.com>
2020-05-14 07:38:38 +0800

    m1

--- ml_play.py
+++ ml_play.py
@@ -2,21 +2,29 @@ import pickle
 from os import path
 
 import numpy as np;
-#import games.pingpong.communication as comm
-#from games.pingpong.communication import (
+# import games.pingpong.communication as comm
+# from games.pingpong.communication import (
 #    SceneInfo, GameStatus, PlatformAction
-#)
+# )
 
 from mlgame.communication import ml as comm
 
-def ml_loop(side: str):
+count = 1
 
+
+def ml_loop(side: str):
     # === Here is the execution order of the loop === #
     # 1. Put the initialization code here
     ball_served = False
-    filename = path.join(path.dirname(__file__), 'save', 'NORMAL_model.pickle')
-    with open(filename, 'rb') as file:
-        clf = pickle.load(file)
+    filename1 = path.join(path.dirname(__file__), 'save', 'side_model.pickle')
+    filename2 = path.join(path.dirname(__file__), 'save', 'helf_model.pickle')
+    filename3 = path.join(path.dirname(__file__), 'save', 'I_model.pickle')
+    with open(filename1, 'rb') as file:
+        clf1 = pickle.load(file)
+    with open(filename2, 'rb') as file:
+        clf2 = pickle.load(file)
+    with open(filename3, 'rb') as file:
+        clf3 = pickle.load(file)
 
     def get_direction(speed_x, speed_y):
         if (speed_x >= 0 and speed_y >= 0):
@@ -27,29 +35,38 @@ def ml_loop(side: str):
             return 2
         elif (speed_x < 0 and speed_y < 0):
             return 3
+
     # 2. Inform the game process that ml process is ready
     comm.ml_ready()
     # 3. Start an endless loop
     while True:
         # 3.1. Receive the scene information sent from the game process
         scene_info = comm.recv_from_game()
-        feature=[]
+        feature = []
         feature.append(scene_info["ball"][0])
         feature.append(scene_info["ball"][1])
-        feature.append(scene_info["platform_1P"][0]+20)
+        feature.append(scene_info["platform_1P"][0] + 20)
 
         feature.append(scene_info["ball_speed"][0])
         feature.append(scene_info["ball_speed"][1])
 
-        feature.append(get_direction(feature[3],feature[4]))
+        feature.append(get_direction(feature[3], feature[4]))
 
+        feature1 = np.array(feature)
+        feature1 = feature1.reshape((-1, 6))
 
-        feature=np.array(feature)
-        feature = feature.reshape((-1,6))
+        if (abs(scene_info["ball"][0] - scene_info["blocker"][0] - 15) <= 20 and abs(
+                scene_info["ball"][1] - scene_info["blocker"][1] - 10) <= 9 and
+                scene_info["ball_speed"][1] > 0):
+            feature.append(1)
+        else:
+            feature.append(0)
+        feature2 = np.array(feature)
+        feature2 = feature2.reshape((-1, 7))
         # 3.2. If either of two sides wins the game, do the updating or
         #      resetting stuff and inform the game process when the ml process
         #      is ready.
-        if scene_info["status"] !="GAME_ALIVE":
+        if scene_info["status"] != "GAME_ALIVE":
             # Do some updating or resetting stuff
             ball_served = False
             # 3.2.1 Inform the game process that
@@ -61,16 +78,26 @@ def ml_loop(side: str):
 
         # 3.4 Send the instruction for this frame to the game process
         if not ball_served:
-            #comm.send_instruction(scene_info.frame, PlatformAction.SERVE_TO_LEFT)
+            # comm.send_instruction(scene_info.frame, PlatformAction.SERVE_TO_LEFT)
             ball_served = True
         else:
-            y = clf.predict(feature)
+            if (abs(scene_info["ball"][0] - scene_info["blocker"][0] - 15) <= 20 and abs(
+                    scene_info["ball"][1] - scene_info["blocker"][1]) <= 21 and
+                    scene_info["ball_speed"][1] <= 0):
+                #print(1)
+                y = clf2.predict(feature1)
+            elif (abs(scene_info["ball_speed"][0]) > scene_info["ball_speed"][1] and scene_info["ball_speed"][1] > 0):
+                y = clf3.predict(feature2)
+                #print(2)
+            else:
+                y = clf1.predict(feature2)
+
             if y == 0:
                 comm.send_to_game({"frame": scene_info["frame"], "command": "NONE"})
-                print('NONE')
-            elif y ==1:
+                # print('NONE')
+            elif y == 1:
                 comm.send_to_game({"frame": scene_info["frame"], "command": "MOVE_LEFT"})
-                print('LEFT')
-            elif y ==2:
+                # print('LEFT')
+            elif y == 2:
                 comm.send_to_game({"frame": scene_info["frame"], "command": "MOVE_RIGHT"})
-                print('RIGHT')
+                # print('RIGHT')
