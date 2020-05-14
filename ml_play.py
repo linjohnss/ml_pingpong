0f5a9f450793b235773b96c1a710ba82ceedc3c5
linjohnss <a0920594352@gmail.com>
2020-05-14 08:42:17 +0800

    ml2

--- ml_play.py
+++ ml_play.py
@@ -19,12 +19,15 @@ def ml_loop(side: str):
     filename1 = path.join(path.dirname(__file__), 'save', 'side_model.pickle')
     filename2 = path.join(path.dirname(__file__), 'save', 'helf_model.pickle')
     filename3 = path.join(path.dirname(__file__), 'save', 'I_model.pickle')
+    filename4 = path.join(path.dirname(__file__), 'save', 'QQ_model.pickle')
     with open(filename1, 'rb') as file:
         clf1 = pickle.load(file)
     with open(filename2, 'rb') as file:
         clf2 = pickle.load(file)
     with open(filename3, 'rb') as file:
         clf3 = pickle.load(file)
+    with open(filename4, 'rb') as file:
+        clf4 = pickle.load(file)
 
     def get_direction(speed_x, speed_y):
         if (speed_x >= 0 and speed_y >= 0):
@@ -55,14 +58,10 @@ def ml_loop(side: str):
         feature1 = np.array(feature)
         feature1 = feature1.reshape((-1, 6))
 
-        if (abs(scene_info["ball"][0] - scene_info["blocker"][0] - 15) <= 20 and abs(
-                scene_info["ball"][1] - scene_info["blocker"][1] - 10) <= 9 and
-                scene_info["ball_speed"][1] > 0):
-            feature.append(1)
-        else:
-            feature.append(0)
+        feature.append(scene_info["blocker"][0])
+        feature.append(scene_info["blocker"][1])
         feature2 = np.array(feature)
-        feature2 = feature2.reshape((-1, 7))
+        feature2 = feature2.reshape((-1, 8))
         # 3.2. If either of two sides wins the game, do the updating or
         #      resetting stuff and inform the game process when the ml process
         #      is ready.
@@ -84,11 +83,17 @@ def ml_loop(side: str):
             if (abs(scene_info["ball"][0] - scene_info["blocker"][0] - 15) <= 20 and abs(
                     scene_info["ball"][1] - scene_info["blocker"][1]) <= 21 and
                     scene_info["ball_speed"][1] <= 0):
-                #print(1)
+                print(1)
                 y = clf2.predict(feature1)
+            elif(abs(scene_info["ball"][0] - scene_info["blocker"][0] - 15) <= 20 and abs(
+                scene_info["ball"][1] - scene_info["blocker"][1] - 10) <= 9 and
+                scene_info["ball_speed"][1] > 0):
+                y=clf4.predict(feature2)
+                print(2)
+
             elif (abs(scene_info["ball_speed"][0]) > scene_info["ball_speed"][1] and scene_info["ball_speed"][1] > 0):
                 y = clf3.predict(feature2)
-                #print(2)
+                print(3)
             else:
                 y = clf1.predict(feature2)
 
