a64bf39d41e133dcf8d2fb11b2e21e27657bce57
linjohnss <a0920594352@gmail.com>
2020-05-16 00:32:17 +0800

    ml_5

--- ml_play.py
+++ ml_play.py
@@ -11,7 +11,7 @@ def ml_loop(side: str):
     # 1. Put the initialization code here
     ball_served = False
     filename1 = path.join(path.dirname(__file__), 'save', 'NORMAL_model.pickle')
-    filename2 = path.join(path.dirname(__file__), 'save', 'helf_model.pickle')
+    filename2 = path.join(path.dirname(__file__), 'save', 'half_model.pickle')
     filename3 = path.join(path.dirname(__file__), 'save', 'IL_model.pickle')
     filename4 = path.join(path.dirname(__file__), 'save','IR_model.pickle')
 
